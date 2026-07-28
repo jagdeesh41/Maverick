b872cab0c78e8b5780ecb3d8c6ed1baffd9666f3
JavaBot <71251158+jagdeesh41@users.noreply.github.com>
2026-07-28 14:59:11 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useMemo } from 'reactt';
+import { createContext, useContext, useState, useMemo } from 'react';
 import { USERS } from '../mock/mockData';
 
 const AuthContext = createContext(null);
